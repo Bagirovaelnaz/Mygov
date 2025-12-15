bb54f05207567ebaca9d6f150fcbfefad2606f81
Bagirova Elnaz <154632330+Bagirovaelnaz@users.noreply.github.com>
2025-12-16 00:36:25 +0400

    Heyat Hadiseleri section

--- app/components/home/HeyatHadiseleri.tsx
+++ app/components/home/HeyatHadiseleri.tsx
@@ -1,84 +1,135 @@
 const HeyatHadiseleri = () => {
   return (
     <div className="container mx-auto">
-      <h3>Həyat hadisələri</h3>
-      <button className="button-7 uppercase">Bütün xidmətlər</button>
-      <div className="">
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Ail%C9%99-qurmaq.svg"
-            alt=""
-          />
+      <div className=" flex justify-between  px-5">
+        <h2>Həyat hadisələri</h2>
+        <button className="button-7 uppercase">Bütün xidmətlər</button>
+      </div>
+
+      <div className="mt-[23px] mx-4 grid grid-cols-2 gap-3 md:gap-[18px] lg:grid-cols-3 xl:grid-cols-4">
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Ail%C9%99-qurmaq.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
+
           <h4>Ailə qurmaq</h4>
         </div>
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Valideyn-olmaq.svg"
-            alt=""
-          />
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            {" "}
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Valideyn-olmaq.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
           <h4>Valideyn olmaq</h4>
-        </div>{" "}
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Ail%C9%99-%C3%BCzv%C3%BCn%C3%BCn-v%C9%99fat%C4%B1.svg"
-            alt=""
-          />
+        </div>
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Ail%C9%99-%C3%BCzv%C3%BCn%C3%BCn-v%C9%99fat%C4%B1.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
           <h4>Ailə üzvünün vəfatı</h4>
         </div>{" "}
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/T%C9%99hsil-almaq.svg"
-            alt=""
-          />
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            {" "}
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/T%C9%99hsil-almaq.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
+
           <h4>Təhsil almaq</h4>
         </div>{" "}
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Sa%C4%9Flaml%C4%B1q.svg"
-            alt=""
-          />
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            <img
+              className="p-3.5"
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Sa%C4%9Flaml%C4%B1q.svg"
+              alt=""
+            />
+          </div>
+
           <h4>Sağlamlıq</h4>
         </div>
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/M%C9%99%C5%9F%C4%9Fulluq.svg"
-            alt=""
-          />
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/M%C9%99%C5%9F%C4%9Fulluq.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
+
           <h4>Məşğulluq</h4>
         </div>
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/T%C9%99qa%C3%BCd%C9%99-%C3%A7%C4%B1xmaq.svg"
-            alt=""
-          />
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            {" "}
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/T%C9%99qa%C3%BCd%C9%99-%C3%A7%C4%B1xmaq.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
+
           <h4>Təqaüdə çıxmaq</h4>
         </div>
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Da%C5%9F%C4%B1nmaz-%C9%99mlak-almaq.svg"
-            alt=""
-          />
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Da%C5%9F%C4%B1nmaz-%C9%99mlak-almaq.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
+
           <h4>Daşınmaz əmlak almaq</h4>
         </div>
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/N%C9%99qliyyat-vasit%C9%99si-almaq.svg"
-            alt=""
-          />
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            {" "}
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/N%C9%99qliyyat-vasit%C9%99si-almaq.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
+
           <h4>Nəqliyyat vasitəsi almaq</h4>
         </div>
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Az%C9%99rbaycana-k%C3%B6%C3%A7m%C9%99k.svg"
-            alt=""
-          />
-          <h4></h4>
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            {" "}
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Az%C9%99rbaycana-k%C3%B6%C3%A7m%C9%99k.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
+
+          <h4>Azərbaycana köçmək</h4>
         </div>
-        <div>
-          <img
-            src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Qaraba%C4%9Fa-qay%C4%B1d%C4%B1%C5%9F.svg"
-            alt=""
-          />
+        <div className="w-full transform rounded-[24px] bg-[#F7F7F7] p-[22px_22px] transition-all duration-300 hover:scale-[1.04]  md:gap-[16px] md:p-[22px_22px] lg:p-[22px_22px] xl:p-[22px_22px]">
+          <div className="mb-5 inline-block rounded-full border-[2px] border-[#A6A29833] transition-all duration-300 group-hover:border-[#80b7ef]">
+            {" "}
+            <img
+              src="https://mygov-cdn.e-gov.az/cdn/life-events/web/Qaraba%C4%9Fa-qay%C4%B1d%C4%B1%C5%9F.svg"
+              alt=""
+              className="p-3.5"
+            />
+          </div>
+
           <h4>Qarabağa qayıdış</h4>
         </div>
       </div>
